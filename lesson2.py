c06eef1111750820d013c8462a3c4c8d95c9f459
Pinal <arpinal@cps.k12.il.us>
2024-08-30 14:52:12 -0500

    first commit

---
+++ lesson2.py
@@ -0,0 +1,28 @@
+# print ("Hello World")
+# day_of_week=input("What day of the week is it today? ")
+# print("Today is "+ day_of_week)
+
+# moviesthisweek = input ("What movies are you watching this week?")
+# print("I am watching " + moviesthisweek + " This week" )
+# mood=input("How are you feeling today?")
+# print("I am feeling " + mood " today")
+
+# name=  "John"
+# year="2024"
+# yearfourfromnow=2028
+# subtract= yearfourfromnow - year
+# print(subtract)
+
+# pricebigmac= 3.99
+# pricequarter= 4.99
+# totalprice= pricebigmac + pricequarter
+# print(totalprice)
+
+movie=input("what movie are you seeing?'")
+movietime=input("What time is the movie you are seeing?")
+dinner=input("where is your dinner going to be?")
+dinnertime=input("what time is your dinner?")
+dinnerprice=input("How much is your dinner?")
+movieprice=input("How much is your movie?")
+totalprice= dinnerprice + movieprice 
+print("Today my family and I will see " + movie + " at " + movietime + ". Then, we will go out to eat at " + dinner + " at " + dinnertime + " The total price for the night will be " + totalprice)
